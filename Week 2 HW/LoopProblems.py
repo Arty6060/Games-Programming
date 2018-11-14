44a6688aa6627d1318dcff50daef90655d37b15a
Arty6060 <1712011@brunel.ac.uk>
2018-11-14 14:29:03 +0000

    Week 2 work update

--- Week 2 HW/LoopProblems.py
+++ Week 2 HW/LoopProblems.py
@@ -1,5 +1,25 @@
 print("Problem 1")
 
 wordList = ["xxx", "aaa", "r5t6yy", "g", "wow"]
-for wordList in range():
-    print(wordList)
+
+count = 0
+
+for word in wordList:
+    if len(word) >=2 and word[0] == word[-1]:
+        count = count + 1
+
+print(count)
+
+print("Problem 2")
+
+numberList = [2, 3, 5, 7, 66, 89, 134]
+
+newList = []
+
+userInput = int(input("Type a number here -  "))
+
+for number in numberList:
+    if (number < userInput):
+        newList.append(number)
+
+print(newList)
